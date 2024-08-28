cfea04030807f6aa44b9d312e14becd7acb7376c
Saifulla Jubair <saifullajubair6@gmail.com>
2024-08-28 18:21:34 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,18 @@ const App = () => {
 
   useEffect(() => {
     const getUser = () => {
-      fetch("http://localhost:5000/auth/login/success", {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
-        },
-      })
+      fetch(
+        "https://googleauthbackend-judrv.ondigitalocean.app/auth/login/success",
+        {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Credentials": true,
+          },
+        }
+      )
         .then((response) => {
           if (response.status === 200) return response.json();
           throw new Error("authentication has been failed!");
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 
 const Navbar = ({ user }) => {
   const logout = () => {
-    window.open("http://localhost:5000/auth/logout", "_self");
+    window.open(
+      "https://googleauthbackend-judrv.ondigitalocean.app/auth/logout",
+      "_self"
+    );
   };
   return (
     <div className="navbar">
